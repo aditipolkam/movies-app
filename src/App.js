6de3ab1ce0af4de6af9d63876735aa70ea8c5fe5
Aditi Polkam <aditipolkam@gmail.com>
2022-03-22 13:59:39 +0530

    [UPDATE] display movie components mapping

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import MovieCard from './MovieCard';
 
 // cdfaaf13
 const API_URL = 'http://www.omdbapi.com?apikey=cdfaaf13'
-const movie = {
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    "Title": "Batman Begins",
-    "Type": "movie",
-    "Year": "2005",
-    "imdbID": "tt0372784"
-}
 
 const App = () => {
 
@@ -38,15 +31,20 @@ const App = () => {
             </div>
 
             {
-                movies.length > 0 ? (
-                    <div className='container'>
-                        <MovieCard movie={movies[0]} />
-                    </div>
-                ) : (
-                    <div className='empty'>
-                        <h2>No movies found</h2>
-                    </div>
-                )
+                movies?.length > 0
+                    ? (
+                        <div className='container'>
+                            {
+                                movies.map((movie) => (
+                                    <MovieCard movie={movie} />
+                                ))
+                            }
+                        </div>
+                    ) : (
+                        <div className='empty'>
+                            <h2>No movies found</h2>
+                        </div>
+                    )
             }
 
         </div>
