a62b02dbae4f0643c0bacf281abc81f08059908d
Aditi Polkam <aditipolkam@gmail.com>
2022-03-22 14:06:10 +0530

    [MODIFY] search bar functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=cdfaaf13'
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -25,8 +26,15 @@ const App = () => {
 
             <div className='search'>
 
-                <input type='text' placeholder='Search for a movie' value="Superman" onChange={() => { }} />
-                <img src={SearchIcon} alt='Search' onClick={() => { }} />
+                <input
+                    type='text'
+                    placeholder='Search for a movie'
+                    value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+
+                <img
+                    src={SearchIcon}
+                    alt='Search'
+                    onClick={() => { searchMovies(searchTerm) }} />
 
             </div>
 
