c9798337625c5a7ed1b447621969b81357011dd9
Aditi Polkam <aditipolkam@gmail.com>
2022-03-22 13:44:54 +0530

    [ADD] MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
 // cdfaaf13
 const API_URL = 'http://www.omdbapi.com?apikey=cdfaaf13'
 const movie = {
@@ -34,20 +36,7 @@ const App = () => {
             </div>
 
             <div className='container'>
-                <div className='movie'>
-                    <div>
-                        <p>{movie.Year}</p>
-                    </div>
-                    <div>
-                        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
-                    </div>
-                    <div>
-                        <span>
-                            {movie.Type}
-                        </span>
-                        <h3>{movie.Title}</h3>
-                    </div>
-                </div>
+                <MovieCard movie={movie} />
             </div>
         </div>
     );
